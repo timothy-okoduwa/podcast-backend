e7b5cd8518fccd1d9d76218400f7592039a26ec3
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-02-06 13:05:17 +0100

    abeg

--- config/database.js
+++ config/database.js
@@ -1,33 +1,13 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-// path: ./config/env/production/database.js
-
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
+
 
