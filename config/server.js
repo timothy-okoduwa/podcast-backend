8bd5f9dd0191c288a3d31644d555215e8cd479a7
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-02-06 10:37:55 +0100

    app

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-  keys: ['myKeyA', 'myKeyB']
+    keys: [
+      "vnkP/vUK9m4m19QZYychgg==",
+      "jmiIoqtK0ooV6NVB79LScQ==",
+      "W/psnyl8pFw2irpeXTciGQ==",
+      "9RlKXJbZikhprAElczQP4w==",
+    ],
   },
 });
