a205566177d34f04b1dfccf0cbf9f7afcc275536
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-02-06 13:01:21 +0100

    omoh

--- config/database.js
+++ config/database.js
@@ -9,23 +9,25 @@
 //     useNullAsDefault: true,
 //   },
 // });
-const { parse } = require("pg-connection-string");
+// path: ./config/env/production/database.js
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-      debug: false,
     },
-  };
-};
+    debug: false,
+  },
+});
+
