6c2acde2ac376262f79add666cd5c4ab5b4c3c4e
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-02-06 12:26:55 +0100

    ssl

--- config/database.js
+++ config/database.js
@@ -1,11 +1,31 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
